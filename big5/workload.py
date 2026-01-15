9a459ae76aa35ca86f67b8e933eeee4af1c9ed03
Rishabh Singh <sngri@amazon.com>
2026-01-15 10:16:17 -0800

    refresh big5

--- big5/workload.py
+++ big5/workload.py
@@ -1,4 +1,23 @@
+import random
 from .runners import register as register_runners
+
+
+class RandomProcessNameParamSource:
+    def __init__(self, workload, params, **kwargs):
+        self._params = params
+        self.infinite = True
+        self.process_names = ["udev", "systemd", "sshd", "kernel", "journal", "httpd", "cron"]
+        random.seed(42)
+    
+    def partition(self, partition_index, total_partitions):
+        return self
+    
+    def params(self):
+        return {
+            "process_name": random.choice(self.process_names)
+        }
+
 
 def register(registry):
     register_runners(registry)
+    registry.register_param_source("random-process-name-source", RandomProcessNameParamSource)
